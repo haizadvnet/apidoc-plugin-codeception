15c179e513359d56f84ffa7304f6df3fc0fa97e8
Haizad <haizad@vn.my>
2021-11-18 15:48:38 +0800

    Beta apidoc-plugin-codeception

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ apidoc plugin to export codeception API test format
 npm i haizadvnet/apidoc-plugin-codeception -g
 ```
 
+```ApiDocCest.php``` will be generated once you generate your apidoc.
+
+** Feel free to contribute! I really appreciate your contribution
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let codeceptionMethod = []
 let codeceptionIsJSON = []
 let codeceptionParam = []
 let counter = 0
+let to_append_function = 0 //To make function name unique
 /**
  * Hook overview: https://github.com/apidoc/apidoc-core/hooks.md
  */
@@ -48,17 +49,12 @@ function parserFindElements(elements, element, block, filename) {
         APIname = parsedElement.title
       }
 
-      codeceptionName[counter] =  String(APIname).replace(/[\W_]/g, "") + '_' +counter
+      codeceptionName[counter] =  String(APIname).replace(/[\W_]/g, "")
       codeceptionURL[counter] =  parsedElement.url
       codeceptionMethod[counter] =  pushMethod
       codeceptionIsJSON[counter] = true // hardcoded JSON
-      // codeceptionParam[counter] = JSON.parse(parsedElement.content)
     }
 
-    // check for supported elements
-    // if (element.name && Object.keys(converter.ConvertersMap).includes(element.name)) {
-    //   codeceptionMethod[counter] = converter.resolve(element.name).init(parsedElement, codeceptionMethod[counter])
-    // }
   } catch (err) {
     console.error('OOPS! errored element: '+JSON.stringify(element.name)+' in filename: '+filename)
     console.error(err)
@@ -89,22 +85,17 @@ process.on('exit', (code) => {
 
     function myFunction(item, index) {
 
-      docs += 'public function ' + item + '(\\ApiTester $I)\r\n{\r\n$I->haveHttpHeader(\'accept\', \'application/json\'); '
-      docs += '\r\n$I->haveHttpHeader(\'content-type\', \'application/json\');\r\n$I->haveHttpHeader(\'content-type\', '
-      docs += '\'application/json\');\r\n$I->sendPost(\''+codeceptionURL[index]+'\', '
+      to_append_function++;
+
+      docs += 'public function ' + item + '_' + to_append_function + '(\\ApiTester $I)\r\n{\r\n$I->haveHttpHeader(\'accept\', \'application/json\'); '
+      docs += '\r\n$I->haveHttpHeader(\'content-type\', \'application/json\');\r\n$I->sendPost(\''+codeceptionURL[index]+'\', '
       docs += paramArray
       docs += ');'
       docs += '\r\n$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);'
       docs += '\r\n$I->seeResponseIsJson();\r\n}\r\n\r\n'
     }
 
-    fs.writeFileSync(destinationFilePath, '<?php\r\n\r\nclass ApiDocCest\r\n{\r\n'+docs+'\r\n}\r\n\r\n?>', function (err) {
-      if (err) throw err;
-      console.log(`[apidoc-plugin-codeception] ApiDocCest.php file saved successfully`)
-    });
-
-    // fs.writeFileSync(destinationFilePath, JSON.stringify(swaggerObj, null, 2), 'utf8')
-    
-    // console.log(`[apidoc-plugin-codeception] ApiDocCest.php file saved successfully`)
+    fs.writeFileSync(destinationFilePath, '<?php\r\n\r\nclass ApiDocCest\r\n{\r\n'+docs+'\r\n}\r\n\r\n?>')
+    console.log(`[apidoc-plugin-codeception] ApiDocCest.php file saved successfully`)
   }
 })
